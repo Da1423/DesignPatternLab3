d7dd0b10893d9bc6b84b0e282320288107304fee
Da1423 <131502483+Da1423@users.noreply.github.com>
2024-03-08 01:26:46 +0300

    Add files via upload

---
+++ GameLevel.java
@@ -0,0 +1,54 @@
+package designpatternlab3;
+
+/**
+ *
+ * Dana Alasmari
+ * 2116655
+ */
+interface GameLevel extends Cloneable {
+    GameLevel clone();
+    void configure();
+}
+
+// Concrete class
+class BasicLevel implements GameLevel {
+
+    @Override
+    public GameLevel clone() {
+        return new BasicLevel();
+    }
+
+    @Override
+    public void configure() {
+        System.out.println("Basic Level");
+    }
+}
+
+// Concrete class
+class AdvancedLevel implements GameLevel {
+
+    @Override
+    public GameLevel clone() {
+        return new AdvancedLevel();
+    }
+
+    @Override
+    public void configure() {
+        System.out.println("Advanced Level");
+    }
+}
+
+class LevelDesignTool {
+
+    public static void main(String[] args) {
+
+        GameLevel basicLevelPrototype = new BasicLevel();
+        GameLevel basic = basicLevelPrototype.clone();
+        basic.configure();
+
+        GameLevel advancedLevelPrototype = new AdvancedLevel();
+        GameLevel advanced = advancedLevelPrototype.clone();
+        advanced.configure();
+
+    }
+}
